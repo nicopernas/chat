208a784d76f6ad420039cbf8590f7caf94e00236
Nicolás Pernas Maradei <nicolas@polymerlabs.org>
2022-07-27 15:59:37 +0200

    add first message id to genesis

--- x/chat/types/genesis.go
+++ x/chat/types/genesis.go
@@ -11,8 +11,10 @@ const DefaultIndex uint64 = 1
 // DefaultGenesis returns the default Capability genesis state
 func DefaultGenesis() *GenesisState {
 	return &GenesisState{
-		PortId:        PortID,
-		NextMessageId: nil,
+		PortId: PortID,
+		NextMessageId: &NextMessageId{
+			MessageId: DefaultIndex,
+		},
 		// this line is used by starport scaffolding # genesis/types/default
 		Params: DefaultParams(),
 	}
