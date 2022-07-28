4880ba762adfd90907943ad584a81bb76f8814e7
Nicolás Pernas Maradei <nicolas@polymerlabs.org>
2022-07-28 09:55:01 +0200

    store message

--- x/chat/keeper/msg_server_new_message.go
+++ x/chat/keeper/msg_server_new_message.go
@@ -2,6 +2,7 @@ package keeper
 
 import (
 	"context"
+	"strconv"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/nicopernas/chat/x/chat/types"
@@ -10,8 +11,23 @@ import (
 func (k msgServer) NewMessage(goCtx context.Context, msg *types.MsgNewMessage) (*types.MsgNewMessageResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	// TODO: Handling the message
-	_ = ctx
+	nextMessageId, found := k.GetNextMessageId(ctx)
+	if !found {
+		panic("could not find nextMessageId")
+	}
 
-	return &types.MsgNewMessageResponse{}, nil
+	messageId := strconv.FormatUint(nextMessageId.MessageId, 10)
+
+	k.SetMessage(ctx, types.Message{
+		Creator:   msg.Creator,
+		MessageId: messageId,
+		Body:      msg.Body,
+	})
+
+	nextMessageId.MessageId++
+	k.SetNextMessageId(ctx, nextMessageId)
+
+	return &types.MsgNewMessageResponse{
+		MessageId: messageId,
+	}, nil
 }
